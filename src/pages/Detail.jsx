3f36bc2784cfa905917d6f2ed70efe6a2645a6a2
unknown <choipoe@gmail.com>
2024-06-24 15:54:28 +0900

    실패

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { todoApi } from "../api/todos";
+import axios from "axios";
 
 export default function Detail() {
   const { id } = useParams();
@@ -9,32 +10,41 @@ export default function Detail() {
 
   // TODO: useQuery 로 리팩터링 하세요.
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchDetail = async () => {
-      try {
-        const response = await todoApi(`/todos/${id}`);
-        setData(response.data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  const fetchDetail = async () => {
+    const response = await todoApi.get(`/todos/${id}`);
+    return response.data;
+  };
 
-    fetchDetail();
-  }, [id]);
+  const {
+    data: todos,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ["todos", id],
+    queryFn: fetchDetail,
+  });
 
-  if (isLoading) return <div style={{ fontSize: 36 }}>로딩중...</div>;
-  if (error) {
-    console.error(error);
-    return (
-      <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
-    );
+  console.log(data); // undefined
+  console.log(todos); // undefined
+
+  if (isPending) {
+    alert("데이터로딩중");
   }
+
+  if (isError) {
+    alert("오류!");
+  }
+  // if (isLoading) return <div style={{ fontSize: 36 }}>로딩중...</div>;
+  // if (error) {
+  //   console.error(error);
+  //   return (
+  //     <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
+  //   );
+  // }
 
   return (
     <div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,38 +3,55 @@ import { useNavigate } from "react-router-dom";
 import { todoApi } from "../api/todos";
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function Home() {
   // TODO: useQuery 로 리팩터링 하세요.
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
+
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // const [data, setData] = useState([]);
 
   const fetchData = async () => {
-    try {
-      const response = await todoApi.get("/todos");
-      setData(response.data);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setIsLoading(false);
-    }
+    const response = await todoApi.get("/todos");
+    return response.data;
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const {
+    data: todos,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchData,
+  });
 
-  if (isLoading) {
-    return <div style={{ fontSize: 36 }}>로딩중...</div>;
+  console.log(data); // undefined
+  console.log(todos); // undefined
+
+  if (isPending) {
+    alert("데이터로딩중");
   }
 
-  if (error) {
-    console.error(error);
-    return (
-      <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
-    );
+  if (isError) {
+    alert("오류!");
   }
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // if (isLoading) {
+  //   return <div style={{ fontSize: 36 }}>로딩중...</div>;
+  // }
+
+  // if (error) {
+  //   console.error(error);
+  //   return (
+  //     <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
+  //   );
+  // }
 
   return (
     <>
